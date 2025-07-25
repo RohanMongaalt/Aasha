83f731f88d2c7bccfb7223d616bfaa731e478fbd
RohanMongaalt <rohanmongaalt@gmail.com>
2025-07-25 15:46:54 +0530

    Updated GoalsTasksPage.tsx

--- src/pages/GoalsTasksPage.tsx
+++ src/pages/GoalsTasksPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
